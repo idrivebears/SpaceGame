dd728af4736c7495e307d674039d1762870d59d0
Alex Walls <idrivebears@gmail.com>
2014-10-17 14:30:24 -0500

    Added Networking/ClientMessage

---
+++ client/src/Networking/ClientMessage.java
@@ -0,0 +1,18 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Networking;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Usuario
+ */
+public class ClientMessage {
+    
+    ArrayList<String> pressedKeys = new ArrayList<String>();
+    
+    
+}
