4f47f27679102c3f40d91d14202aa6a210169732
Alex Walls <idrivebears@gmail.com>
2014-10-17 14:32:47 -0500

    Changes

--- client/src/Networking/ClientMessage.java
+++ client/src/Networking/ClientMessage.java
@@ -1,18 +1,21 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package Networking;
 
 import java.util.ArrayList;
 
 /**
  *
- * @author Usuario
+ * @author alexwalls
  */
 public class ClientMessage {
     
-    ArrayList<String> pressedKeys = new ArrayList<String>();
+    private String playerID;
+    private ArrayList<String> pressedKeys = new ArrayList<String>();
+    
+    public clientMessage(){
+        playerID = id;
+    }
+    
     
     
 }
