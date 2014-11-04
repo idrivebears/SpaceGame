695f37319c021e45b8a6fb2f56d14b3310cbe942
Jose Antonio Camarena <josean.camarena@gmail.com>
2014-11-04 11:40:30 -0600

    Added client for merging (main branch)

---
+++ client/src/SpaceEntities/VisualPlayer.java
@@ -0,0 +1,42 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package SpaceEntities;
+
+import com.jme3.math.Quaternion;
+import com.jme3.math.Vector3f;
+
+/**
+ *  DEPRECATED
+ * @author awalls
+ * 
+ */
+public class VisualPlayer {
+    private Vector3f direction;
+    private Vector3f position;
+    Quaternion currentAngle;
+    
+    public VisualPlayer(Vector3f position, Vector3f direction, Quaternion angle){
+        this.position = position;
+        this.direction = direction;
+        this.currentAngle = angle;
+    }  
+    
+    public Vector3f getDirection() {
+        return direction;
+    }
+
+    public Vector3f getPosition() {
+        return position;
+    }
+
+    public Quaternion getCurrentAngle() {
+        return currentAngle;
+    }
+
+    
+
+    
+    
+}
---
+++ client/src/SpaceNetworking/ClientMessage.java
@@ -0,0 +1,28 @@
+
+package SpaceNetworking;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author alexwalls
+ */
+public class ClientMessage {
+    
+    private String playerID;
+    private String keyPressed;
+    
+    public ClientMessage(String id, String keyPressed){
+        playerID = id;
+        this.keyPressed = keyPressed;
+    }
+    public String getID(){
+        return playerID;
+    }
+    public String getPressedKeys(){
+        return keyPressed;
+    }
+    
+    
+    
+}
