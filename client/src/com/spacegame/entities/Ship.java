d75b9f0f578301cbce53994a19532a5de0fc976e
José Ignacio González Cárdenas <nacho.gon.car@gmail.com>
2014-11-07 12:59:48 -0600

    ActionListener
    No more analog :(

--- client/src/com/spacegame/entities/Ship.java
+++ client/src/com/spacegame/entities/Ship.java
@@ -15,30 +15,56 @@ import com.jme3.math.Vector3f;
  * @author gwAwr
  */
 public class Ship extends Element{
+    
+    private float speed = 32f; //default speed
+    private float pitchSpeed = 0; 
+    private float rollSpeed = 0;
+    
     public Ship(String model, AssetManager am){
         elementData = new ElementData();
         spatial = am.loadModel(model);
         this.currentNode.attachChild(spatial);
     }
+    
+    public void setSpeed(float speed){
+        this.speed = speed;
+    }
+    public float getSpeed(){
+        return speed;
+    }
+    
+    public void setPitchSpeed(float s){
+        this.pitchSpeed = s;
+    }
+    public float getPitchSpeed(){
+        return this.pitchSpeed;
+    }
+    public void setRollSpeed(float s){
+        this.rollSpeed = s;
+    }
+    public float getRollSpeed(){
+        return this.rollSpeed;
+    }
+    
     ///Movement
     // Around x axe. Positive
     public void PitchUp(float tpf){
-        Quaternion PITCH = new Quaternion().fromAngleAxis((FastMath.PI*tpf)/10, new Vector3f(1,0,0));
+        Quaternion PITCH = new Quaternion().fromAngleAxis((FastMath.PI * tpf * pitchSpeed)/10, new Vector3f(1,0,0));
         this.setLocalRotation(this.getLocalRotation().mult(PITCH)); 
     }
     // Around x axe. Negative
     public void PitchDown(float tpf){
-        Quaternion PITCH = new Quaternion().fromAngleAxis((FastMath.PI*tpf)/10, new Vector3f(-1,0,0));
+        Quaternion PITCH = new Quaternion().fromAngleAxis((FastMath.PI * tpf * pitchSpeed)/10, new Vector3f(1,0,0));
         this.setLocalRotation(this.getLocalRotation().mult(PITCH));
     }
     // Around z axe. Positive
     public void RollRight(float tpf){
-        Quaternion ROLL = new Quaternion().fromAngleAxis((-FastMath.PI*tpf)/5, new Vector3f(0,0,1));
+        Quaternion ROLL = new Quaternion().fromAngleAxis((FastMath.PI * tpf * rollSpeed)/5, new Vector3f(0,0,1));
         this.setLocalRotation(this.getLocalRotation().mult(ROLL));
     }
     // Around z axe. Negative
     public void RollLeft(float tpf){
-        Quaternion ROLL = new Quaternion().fromAngleAxis((FastMath.PI*tpf)/5, new Vector3f(0,0,1));
+        Quaternion ROLL = new Quaternion().fromAngleAxis((FastMath.PI * tpf * rollSpeed)/5, new Vector3f(0,0,1));
         this.setLocalRotation(this.getLocalRotation().mult(ROLL));
     }
     
--- client/src/com/spacegame/util/InputHandler.java
+++ client/src/com/spacegame/util/InputHandler.java
@@ -1,10 +1,8 @@
 package com.spacegame.util;
 
+import com.jme3.input.controls.ActionListener;
 import com.spacegame.entities.Player;
 import com.jme3.input.controls.AnalogListener;
-import com.jme3.math.FastMath;
-import com.jme3.math.Quaternion;
-import com.jme3.math.Vector3f;
 
 /**
  *
@@ -12,7 +10,7 @@ import com.jme3.math.Vector3f;
  * 
  * This will send the input pressed by the client to the server, including a client ID
  */
-public class InputHandler implements AnalogListener{
+public class InputHandler implements AnalogListener, ActionListener {
     Player thisPlayer;
     
     public InputHandler(Player player){
@@ -23,31 +21,40 @@ public class InputHandler implements AnalogListener{
     @Override
     public void onAnalog(String name, float value, float tpf){
         // Basic movement. Considers "tpf", making velocity in all directions equal
-       // if(name.equals("INPUT_Forward")){
-            //Vector3f mov = ship.getLocalRotation().getRotationColumn(2).normalize();
-            
-         //   Vector3f mov = thisPlayer.getLocalRotation().getRotationColumn(2).normalize();
-          //  thisPlayer.setDirection(mov.mult(-thisPlayer.getSpeed()));
-        //}
-//        if(name.equals("INPUT_Backward")){
-//            Vector3f mov = thisPlayer.getLocalRotation().getRotationColumn(2).normalize();
-//            thisPlayer.setDirection(mov.mult(thisPlayer.getSpeed()));
-//        }
-        //System.out.println(value + " " + tpf); value is equal to tpf
+
+        //System.out.println(value + " " + tpf); //value is equal to tpf
+        /*
         if(name.equals("INPUT_RollUp")){
+            
             thisPlayer.PitchUp(tpf);
         }
         if(name.equals("INPUT_RollDown")){
             thisPlayer.PitchDown(tpf);
-        }/*
-        if(name.equals("INPUT_Right")){
-            Vector3f mov = thisPlayer.getLocalRotation().getRotationColumn(0).normalize();
-            thisPlayer.setDirection(mov.mult(thisPlayer.getSpeed()));
         }
-        if(name.equals("INPUT_Left")){
-            Vector3f mov = thisPlayer.getLocalRotation().getRotationColumn(0).normalize();
-            thisPlayer.setDirection(mov.mult(-thisPlayer.getSpeed()));
-        }*/
+        if(name.equals("INPUT_RollLeft")){
+            thisPlayer.RollLeft(tpf);
+            
+        }
+        if(name.equals("INPUT_RollRight")){
+            thisPlayer.RollRight(tpf);
+        }
+        if(name.equals("INPUT_Shoot")){
+            thisPlayer.Shoot();
+        }
+        */
+    }
+
+    public void onAction(String name, boolean isPressed, float tpf) {
+        System.out.println(name + " " + isPressed + " " + tpf);
+        
+                
+        if(name.equals("INPUT_RollUp")){
+            
+            thisPlayer.PitchUp(tpf);
+        }
+        if(name.equals("INPUT_RollDown")){
+            thisPlayer.PitchDown(tpf);
+        }
         if(name.equals("INPUT_RollLeft")){
             thisPlayer.RollLeft(tpf);
             
