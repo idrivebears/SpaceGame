f73750342f09220cc41cd6f5cbc443901f1b7b03
José Ignacio González Cárdenas <nacho.gon.car@gmail.com>
2014-11-28 10:21:15 -0600

    Revert "Small cleanup"
    This reverts commit d432858f976803d72b3e52176f072b40e23022db.

--- client/src/com/spacegame/util/Terrain.java
+++ client/src/com/spacegame/util/Terrain.java
@@ -4,8 +4,12 @@
  */
 package com.spacegame.util;
 
+import com.jme3.math.Vector3f;
+import com.jme3.post.FilterPostProcessor;
 import com.jme3.scene.Node;
 import com.jme3.scene.Spatial;
+import com.jme3.texture.Texture2D;
+import com.jme3.water.WaterFilter;
 
 /**
  *
